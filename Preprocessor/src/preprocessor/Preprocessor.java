52af83a45f1abbf307eee03625c1ab47a6e9a1b0
Husni Munaya <husnimun@gmail.com>
2016-11-25 01:59:31 +0700

    Add csv to arff converter

--- Preprocessor/src/preprocessor/Preprocessor.java
+++ Preprocessor/src/preprocessor/Preprocessor.java
@@ -4,6 +4,7 @@ import IndonesianNLP.IndonesianSentenceFormalization;
 import IndonesianNLP.IndonesianStemmer;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -12,6 +13,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NominalToString;
 
 /**
  *
@@ -19,10 +25,10 @@ import java.util.logging.Logger;
  */
 public class Preprocessor {
     public static void main(String[] args) {
-        ArrayList<Instance> data = new ArrayList<>();
+        ArrayList<MyInstance> data = new ArrayList<>();
         try {
             // Read data into csv
-            data = (ArrayList<Instance>) readCSVToList("dataset.csv");
+            data = (ArrayList<MyInstance>) readCSVToList("dataset.csv");
             
             // Preprocessing
             normalizeData(data);
@@ -30,10 +36,13 @@ public class Preprocessor {
             
             // Output
             outputDataToCSV(data, "data-preprocessed");
+//            outputDataToArff("data-preprocessed.csv", "dataset.arff");
 
             
         } catch (IOException ex) {
             Logger.getLogger(Preprocessor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            
         }
     }
     
@@ -44,20 +53,20 @@ public class Preprocessor {
      * @throws java.io.FileNotFoundException 
      * @throws java.io.IOException 
      */
-    public static List<Instance> readCSVToList(String file) throws FileNotFoundException, IOException {
-        List<Instance> data = new ArrayList<>();
+    public static List<MyInstance> readCSVToList(String file) throws FileNotFoundException, IOException {
+        List<MyInstance> data = new ArrayList<>();
         CSVReader reader = new CSVReader(new FileReader(file));
         String[] nextLine;
         
         while ((nextLine = reader.readNext()) != null) {
-            Instance instance = new Instance(nextLine[1], Integer.parseInt(nextLine[3]));
+            MyInstance instance = new MyInstance(nextLine[1], Integer.parseInt(nextLine[3]));
             data.add(instance);
         }
         
         return data;
     }
     
-    public static void stemData(List<Instance> data) {
+    public static void stemData(List<MyInstance> data) {
         IndonesianStemmer stemmer = new IndonesianStemmer();
         for (int i = 0; i < data.size(); i++) {
             String stemmedSentence = stemmer.stemSentence(data.get(i).getTweet());
@@ -69,7 +78,7 @@ public class Preprocessor {
      * Normalize and remove stop words
      * @param data list of Instance
      */
-    public static void normalizeData(List<Instance> data) {
+    public static void normalizeData(List<MyInstance> data) {
         IndonesianSentenceFormalization sentenceFormalization = new IndonesianSentenceFormalization();
         sentenceFormalization.initStopword();
         
@@ -89,12 +98,31 @@ public class Preprocessor {
      * @param filename
      * @throws java.io.IOException
      */
-    public static void outputDataToCSV(List<Instance> data, String filename) throws IOException {
+    public static void outputDataToCSV(List<MyInstance> data, String filename) throws IOException {
         CSVWriter writer = new CSVWriter(new FileWriter(filename + ".csv"));
-        for (Instance instance : data) {
+        String[] header = {"tweet", "class"};
+        writer.writeNext(header);
+        for (MyInstance instance : data) {
             String[] instanceString = {instance.getTweet(), Integer.toHexString(instance.getClassNum())};
             writer.writeNext(instanceString);
         }
+    }
+    
+    public static void outputDataToArff(String csvdata, String filename) throws IOException, Exception {
+        // load CSV
+        CSVLoader loader = new CSVLoader();
+        loader.setSource(new File(csvdata));
+        Instances data = loader.getDataSet();
+        
+        NominalToString filter1 = new NominalToString();
+        filter1.setInputFormat(data);
+        data = Filter.useFilter(data, filter1);
+
+        // save ARFF
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(data);
+        saver.setFile(new File(filename));
+        saver.writeBatch();
     }
     
     public static String removeCurrency(String sentence) {
