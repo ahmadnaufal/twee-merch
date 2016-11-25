8ac766c026e9a92af81125765864e17f4fc34555
fauzanriff <fauzanriff@gmail.com>
2016-11-25 13:17:29 +0700

    Implemented mail information extraction

--- Preprocessor/src/information_extraction/InformationExtraction.java
+++ Preprocessor/src/information_extraction/InformationExtraction.java
@@ -18,6 +18,7 @@ public class InformationExtraction {
     public final static String REGEX_PRICE = "(rp|idr|harga)(\\.|,| )*(\\d)(\\d+|\\.|,)*( ?ribu| ?ratus| ?juta| ?k| ?rb| ?jt)*";
     public final static String REGEX_NOMINAL = "[\\d|\\.]+";
     public final static String REGEX_PHONE = "(08|\\+?628|02\\d)(\\d{9,11})";
+    public final static String REGEX_MAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     
     private InformationSell infoSell;
     private String tweet;
@@ -29,6 +30,12 @@ public class InformationExtraction {
     
     public InformationSell getInformationSell(){
         return infoSell;
+    }
+    
+    public void ieAll(){
+        iePrice();
+        iePhone();
+        ieMail();
     }
     
     public void iePrice(){
@@ -82,5 +89,19 @@ public class InformationExtraction {
         }
         
         infoSell.setPhone(extractPhone);
+    }
+    
+    public void ieMail(){
+        String extractMail;
+        Pattern pattern = Pattern.compile(REGEX_MAIL);
+        Matcher matcher = pattern.matcher(tweet);
+        
+        if(matcher.find()){
+            extractMail = matcher.group(0);
+        }else{
+            extractMail = "-";
+        }
+        
+        infoSell.setEmail(extractMail);
     }
 }
