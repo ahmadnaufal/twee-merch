668b719d5efc5f4c21ca317eeaf5a044c0162292
fauzanriff <fauzanriff@gmail.com>
2016-11-26 14:16:58 +0700

    Fix IE lowecase issue

--- TweeMerchWeb/src/java/information_extraction/InformationExtraction.java
+++ TweeMerchWeb/src/java/information_extraction/InformationExtraction.java
@@ -20,7 +20,7 @@ public class InformationExtraction {
     public final static String REGEX_PHONE = "(08|\\+?628|02\\d)(\\d{9,11})";
     public final static String REGEX_MAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     public final static String REGEX_LINK = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
-    public final static String REGEX_ITEMNAME = "((jual )([a-zA-Z\\d ]+))|(([a-zA-Z\\d \\-\"/\\.\\+\\&\\;]+)(\\.\\.\\.)? http)";
+    public final static String REGEX_ITEMNAME = "(([j|J][u|U][a|A][l|L] )([a-zA-Z\\d ]+))|(([a-zA-Z\\d \\-\"/\\.\\+\\&\\;]+)(\\.\\.\\.)? http)";
     
     public final static String UNIFORM_ALL = "_ALL_";
     public final static String UNIFORM_PRICE = "_PRICE_";
